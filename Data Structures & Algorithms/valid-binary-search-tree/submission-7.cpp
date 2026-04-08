baa35a3cf2c5f7c248d0a15e92dd9f5b87aa9214
Dev Singh <154252296+immmdev@users.noreply.github.com>
2026-04-08 10:38:56 +0530

    Add: valid-binary-search-tree - submission-7

---
+++ Data Structures & Algorithms/valid-binary-search-tree/submission-7.cpp
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    bool validate(TreeNode* root,int minLimit,int maxLimit){
+        if(root==NULL)return true;
+        if(root->val<=minLimit || root->val>=maxLimit)return false;
+        return validate(root->left,minLimit,root->val) && validate(root->right,root->val,maxLimit);
+    }
+    bool isValidBST(TreeNode* root) {
+        
+        return validate(root,INT_MIN,INT_MAX);
+    }
+};
