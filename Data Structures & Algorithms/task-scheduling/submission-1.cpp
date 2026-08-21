0ab165ca401c339448779118b839123bcc84ef33
Dev Singh <154252296+immmdev@users.noreply.github.com>
2026-08-21 12:16:34 +0530

    Add: task-scheduling - submission-1

---
+++ Data Structures & Algorithms/task-scheduling/submission-1.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        map<char,int>f;
+        for(int i=0; i<tasks.size(); i++){
+            f[tasks[i]]++;
+        }
+        // {freq,task}
+        priority_queue<pair<int,char>>pq;
+        for(auto it: f){
+            pq.push({it.second,it.first});
+        }
+        // {task,{freq,callbacktime}}
+        queue<pair<char,pair<int,int>>>q;
+        int time=0;
+        while(!pq.empty() || !q.empty()){
+            time=time+1; // now the task has been executed
+            if(!pq.empty()){
+                auto node=pq.top();
+                pq.pop(); // task gone to the cpu, considering only one unit of time taken by cpu for the task to execute. 
+                int cf=node.first;
+                cf--;
+                if(cf>0)q.push({node.second,{cf,time+n}});
+            }
+            if(!q.empty() && q.front().second.second==time){
+                auto fe=q.front();
+                q.pop();
+                pq.push({fe.second.first,fe.first});
+            }
+
+        }
+        return time;
+
+    }
+};
